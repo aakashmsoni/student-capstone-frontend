cac52cdc68cbc6b2e07c3316576d355f9cec290a
“Scott <stevensscott29@gmail.com>
2023-05-02 22:19:44 -0400

    adding StudentDetailsShow and render from content

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,10 @@
+import { StudentDetailsShow } from "./StudentDetailsShow";
+
 export function Content() {
   return (
     <div>
       <h1>Welcome to React!</h1>
+      <StudentDetailsShow />
     </div>
   );
 }
